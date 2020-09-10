305c9945574ea846b590e6d1b3f3508d809d4952
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2020-09-10 11:44:45 +0900

    test serial monitor
    test serial monitor

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,14 @@
 
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(9600);
 }
  
 void loop() {
-    digitalWrite(LED_BUILTIN, HIGH); 
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("LED On!!"); 
     delay(1000); 
     digitalWrite(LED_BUILTIN, LOW); 
+    Serial.println("LED Off!!");
     delay(1000); 
 }
