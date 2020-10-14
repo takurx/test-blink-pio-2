51b61da0405c48dbe1fbe57b0e6a6bc428dbab9b
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2020-10-14 19:45:43 +0900

    On teensy4.1 testing
    On teensy4.1 testing

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,15 @@
 
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
-    Serial.begin(9600);
+    //Serial.begin(9600);
+    Serial.begin(57600);
 }
  
 void loop() {
     digitalWrite(LED_BUILTIN, HIGH);
     Serial.println("LED On!!"); 
-    delay(1000); 
+    delay(100); 
     digitalWrite(LED_BUILTIN, LOW); 
     Serial.println("LED Off!!");
-    delay(1000); 
+    delay(100); 
 }
