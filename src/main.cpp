c3e3ab3dfb3ba98f1d45c4a4b640630f6667a449
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2021-01-29 01:17:23 +0900

    115200 and symlink
    115200 and symlink

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,20 @@
 #include <Arduino.h>
 
+// for ESP32 devkit v4
+//#define LED_BUILTIN 2
+
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
     //Serial.begin(9600);
-    Serial.begin(57600);
+    //Serial.begin(57600);
+    Serial.begin(115200);
 }
  
 void loop() {
     digitalWrite(LED_BUILTIN, HIGH);
     Serial.println("LED On!!"); 
-    delay(100); 
+    delay(1000); 
     digitalWrite(LED_BUILTIN, LOW); 
     Serial.println("LED Off!!");
-    delay(100); 
+    delay(1000); 
 }
